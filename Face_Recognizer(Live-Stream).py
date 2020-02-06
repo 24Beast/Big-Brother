6d7a6114ba50f7b5078a4a7d43247dd1cae4ce32
24Beast <40471796+24Beast@users.noreply.github.com>
2020-02-06 19:44:23 +0530

    Update Face_Recognizer(Live-Stream).py
    Updated code

--- Face_Recognizer(Live-Stream).py
+++ Face_Recognizer(Live-Stream).py
@@ -1,4 +1,4 @@
-'''Face Recognition Main File'''
+# Importing Necessary Libraries
 import cv2
 import numpy as np
 import pandas as pd
@@ -11,46 +11,44 @@ from inception_blocks_v2 import *
 import dlib
 import requests
 
-#with CustomObjectScope({'tf': tf}):
+# Initializing Face Detection & Recognition Models
 FR_model = load_model('nn4.small2.v1.h5')
-print("Total Params:", FR_model.count_params())
-
-# face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 detector = dlib.get_frontal_face_detector()
 shape_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 threshold = 0.075
 
+# Initializing Face Database
 face_database = {}
-
 id_data = pd.read_csv("Database.csv")
-
 for name in os.listdir('images'):
 	for image in os.listdir(os.path.join('images',name)):
 		identity = os.path.splitext(os.path.basename(image))[0]
 		face_database[identity] = fr_utils.img_path_to_encoding(os.path.join('images',name,image), FR_model)
 
-# Predicting on Live_Stream
 
-# video_capture = cv2.VideoCapture("F:/Projects/SIH/Test5.mp4")
-mem_loc = "F:/Projects/SIH"
+# Initializing Video writers
 frame_H=480
 frame_W=640
-out = cv2.VideoWriter(mem_loc+"/Face_Rec_Video/Test0.avi" ,cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_W,frame_H))        
+out = cv2.VideoWriter("./Face_Rec_Video/Vid_0.avi" ,cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_W,frame_H))
+anomaly = cv2.VideoWriter("./Original_Vid/Vid_0.avi" ,cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_W,frame_H))
+
+# Fetching and Predicting on Live_Stream
 links = []
 names = []
-livestream_url = "http://192.168.139.43:8080/shot.jpg"
-# json_url = "http://192.168.150.46:3001/api/data"
+livestream_url = "http://192.168.139.43:8080/shot.jpg" #edit according to server
+json_url = "http://192.168.150.46:3001/api/data" #edit according to server
 i=0
 while True:
+# Fetching Frame from live stream
     img_resp = requests.get(livestream_url)
     img_arr = np.array(bytearray(img_resp.content),dtype = np.int8)
     frame = cv2.imdecode(img_arr,-1)
     frame = cv2.resize(frame,(640,480))
     link = []
     name_data=[]
-    # frame = cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
-    frame = cv2.flip(frame, 1)  
-    # faces = face_cascade.detectMultiScale(frame, 1.3, 5)
+    frame = cv2.flip(frame, 1) # Correcting for left-right flip while recording
+    anomaly.write(frame)
+# Face Recognition
     faces = detector(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
     for face in faces:
         (x, y, w, h) = face_utils.rect_to_bb(face)
@@ -72,7 +70,6 @@ while True:
             cv2.putText(frame, "Dist : " + str(min_dist), (x, y - 20), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
             name_data.append(identity[:-1].split("_")[0])
             link.append(id_data.loc[id_data["Names"]==identity[:-1].split("_")[0]]["Link"].array)
-            # print(id_data.loc[id_data["Names"]==identity[:-1].split("_")[0]]["Link"].array)
         else:
             cv2.putText(frame, 'No matching faces', (x, y - 20), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255), 2)
             link.append("-1")
@@ -80,23 +77,25 @@ while True:
     cv2.imshow('Face Recognition System', frame)
     if(cv2.waitKey(1) == 27):
         break
-    # print(frame)
     out.write(frame)
     names.append(name_data)
     links.append(link)
     if(i%100==0):
+	anomaly.release()
+	anomaly = cv2.VideoWriter("./Original_Vid/Vid_"+str(int(i/100))+".avi" ,cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_W,frame_H)) 
         out.release()
-        out = cv2.VideoWriter(mem_loc+"/Face_Rec_Video/Test"+str(int(i/100))+".avi" ,cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_W,frame_H)) 
+        out = cv2.VideoWriter("./Face_Rec_Video/Vid_"+str(int(i/100))+".avi" ,cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_W,frame_H))
+# Sending Data for Analytics
     if(i%10 == 0):
         df = pd.DataFrame({"Names":names,"Links":links})
         print(df)
-        # resp = requests.post(json_url, data = df.to_json())
-        # names=[]
-        # links=[]
-        # print(resp.text)
+        resp = requests.post(json_url, data = df.to_json())
+        names=[]
+        links=[]
+        print(resp.text)
     i+=1
 df = pd.DataFrame({"Names":names,"Links":links})
-# resp = requests.post(json_url, data = df.to_json())
-# print(resp.text)
+resp = requests.post(json_url, data = df.to_json())
+print(resp.text)
 cv2.destroyAllWindows()
 out.release()
